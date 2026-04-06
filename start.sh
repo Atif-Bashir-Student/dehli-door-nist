52932beee55f888ddc5902fda122e42cb3213faa
Sajjad Ghaeminejad <sajjadghaemi.n@gmail.com>
2026-04-07 00:13:44 +0300

    finding nemo: where is the folder location

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@ set -x
 cd /opt/render/project/src
 flask --app=sensorhub init-db 2>&1
 flask --app=sensorhub testgen 2>&1
+echo "=== Instance folder contents ==="
+find /opt/render -name "*.db" 2>&1
+find /opt/render -name "instance" -type d 2>&1
 exec gunicorn -w 3 -b 0.0.0.0:10000 "sensorhub:create_app()"
