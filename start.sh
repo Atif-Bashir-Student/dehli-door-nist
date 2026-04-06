49807467a97dddebb5c354da6ccd7f3c9f281278
Sajjad Ghaeminejad <sajjadghaemi.n@gmail.com>
2026-04-07 00:18:40 +0300

    had forgotten to add the masterkey generation to start script

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-set -x
 cd /opt/render/project/src
-flask --app=sensorhub init-db 2>&1
-flask --app=sensorhub testgen 2>&1
-echo "=== Instance folder contents ==="
-find /opt/render -name "*.db" 2>&1
-find /opt/render -name "instance" -type d 2>&1
+flask --app=sensorhub init-db
+flask --app=sensorhub testgen
+flask --app=sensorhub masterkey
 exec gunicorn -w 3 -b 0.0.0.0:10000 "sensorhub:create_app()"
