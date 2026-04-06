b5c92744527829c4488eca6ba7056502f1d736ba
Sajjad Ghaeminejad <sajjadghaemi.n@gmail.com>
2026-04-07 00:09:02 +0300

    Add debug output to start script to see whats failing

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+set -x
 cd /opt/render/project/src
-flask --app=sensorhub init-db
-flask --app=sensorhub testgen
+flask --app=sensorhub init-db 2>&1
+flask --app=sensorhub testgen 2>&1
 exec gunicorn -w 3 -b 0.0.0.0:10000 "sensorhub:create_app()"
